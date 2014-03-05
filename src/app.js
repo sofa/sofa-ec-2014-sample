cb23c129028abff065ceaff41fb90663797c1dae
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2014-03-05 13:46:40 +0100

    adding most simple category browsing

--- src/app.js
+++ src/app.js
@@ -1,4 +1,19 @@
 angular.module('sample', [
     'sdk.services.couchService',
     'sdk.services.basketService'
-]);
+])
+.controller('appController', function($scope, couchService){
+    couchService
+        .getCategory()
+        .then(function(category){
+            $scope.category = category;
+        });
+
+    $scope.goDeeper = function(category){
+        if(category.hasChildren){
+            $scope.category = category;
+        }
+    }
+});
+
+

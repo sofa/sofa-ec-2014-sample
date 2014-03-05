086f7d2d7fca026f10948f81b5c56b7a5ae6653d
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2014-03-05 15:21:09 +0100

    add products view

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 angular.module('sample', [
     'sdk.services.couchService',
-    'sdk.services.basketService'
+    'sdk.services.basketService',
+    'sdk.filter'
 ])
 .controller('appController', function($scope, couchService){
+
+    var states = {
+        CATEGORIES: 0,
+        PRODUCTS: 1,
+        PRODUCT: 2
+    };
+
+    $scope.states = states;
+    $scope.state = states.CATEGORIES;
+
     couchService
         .getCategory()
         .then(function(category){
@@ -13,7 +24,16 @@ angular.module('sample', [
         if(category.hasChildren){
             $scope.category = category;
         }
-    }
+        else {
+
+            couchService
+                .getProducts(category.urlId)
+                .then(function(products){
+                    $scope.products = products;
+                    $scope.state = states.PRODUCTS;
+                });
+        }
+    };
 });
 
 
